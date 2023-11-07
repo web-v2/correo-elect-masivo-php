d674e4fe22389a44848a3c0b017f5a8b8f103762
Samir Vergara V <svergara3@misena.edu.co>
2023-11-07 00:13:47 -0500

    Update sentMail.php

--- app/sentMail.php
+++ app/sentMail.php
@@ -16,8 +16,8 @@ function sent_mail($addres,$cc,$suj,$data, $path){
         $mail->isSMTP();                                            // Send using SMTP
         $mail->Host       = 'smtp.gmail.com';                       // Set the SMTP server to send through
         $mail->SMTPAuth   = true;                                   // Enable SMTP authentication
-        $mail->Username   = 'aplicacionactivosfijos4@gmail.com';        // SMTP username
-        $mail->Password   = 'vighrsthiqaahbyq';                   // SMTP password
+        $mail->Username   = 'tucorreo@gmail.com';        // SMTP username
+        $mail->Password   = '';                   // SMTP password
         $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;         // Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` encouraged
         $mail->Port       = 587;                                    // TCP port to connect to, use 465 for `PHPMailer::ENCRYPTION_SMTPS` above
         $mail->CharSet    = 'UTF-8';
@@ -30,7 +30,7 @@ function sent_mail($addres,$cc,$suj,$data, $path){
         );
 
         //Recipients
-        $mail->setFrom('aplicacionactivosfijos4@gmail.com', 'IRIS SAS');
+        $mail->setFrom('tucorreo@gmail.com', 'IRIS SAS');
         $mail->addAddress($addres, $suj);     // Add a recipient
         //$mail->addCC($cc);
         // Content
